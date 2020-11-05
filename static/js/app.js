b854de1ddee4779d849a159194533376435969db
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-05 15:58:32 -0600

    dropdown menu with forEach

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 // Fetch the JSON data and console log it
 dropdownMenu=d3.select("#selDataset");
+metadataHtml=d3.select("#sample-metadata");
 
 d3.json('samples.json').then(function(data) {
     // select data and save them in variables for next work
@@ -12,9 +13,18 @@ d3.json('samples.json').then(function(data) {
     console.log(samples);
 
     // add ids to dropdown menu:
-    for (var i=0; i < names.length; i++) {
-        dropdownMenu.append("option").attr("value", names[i]).text(`${names[i]}`);
-    };
+    // for (var i=0; i < names.length; i++) {
+    //     dropdownMenu.append("option").attr("value", names[i]).text(`${names[i]}`);
+    // };
+
+    names.forEach(function(id) {
+        dropdownMenu.append("option").attr("value", id).text(`${id}`);
+    });
     
+    
+
+
+
+
 });
 
